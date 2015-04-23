baf75810fc6e0d779333e3b84aa09ac2e373f04e
Jim Ritchie <ritchi51@students.rowan.edu>
2015-04-22 22:29:56 -0400

    Added interrupt code and code for setting new speed of the motors after interrupt being thrown

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include "system.h"
-#include "servo.h"
-#include "DCMotor.h"
-
 /*
  * main.c
  *
@@ -9,30 +6,62 @@
  * Date Submitted: December 16, 2014
  * Latest Version: 20141216
  *
- *
  */
+
+volatile uint8_t speed = 0;
+
 int main(void) {
-     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	 WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-    //Run initialization
-    servoInit();
-    DCMotorInit();
+	//Run initialization
+	servoInit();
+	DCMotorInit();
+	uart_init();
+	_enable_interrupt();
 
-    while(1){
-    	setDCMotorSpeed(127,127);
-    	delay(100000);
-		//Move to upper bound
-		servoSetDuty(DUTY_LOWER_BOUND);
-		//Add delay
-		delay(100000);
-		//Move to upper bound
-		servoSetDuty(DUTY_UPPER_BOUND);
-		//delay
-		delay(100000);
-    	//setDCMotorSpeed(-50,-50);
-    	//delay(100000);
-    	//setDCMotorSpeed(0,0,0,0);
-    	//delay(1);
-    }
+	uint8_t lastSpeed;
+	lastSpeed = speed;
+
+	while(1){
+		while (lastSpeed == speed); // wait for speed modification
+			if (speed > 50) {
+				setDCMotorSpeed(SCALE * (speed - 50));
+				setDCMotorDirection(FORWARD);
+				//uart_send(speed);
+			} else if (speed < 50) {
+				setDCMotorSpeed(SCALE * (50 - speed));
+				setDCMotorDirection(REVERSE);
+				//uart_send(speed);
+			} else {
+				setDCMotorSpeed(0);
+				setDCMotorDirection(BRAKE);
+				//uart_send(speed);
+			}
+		lastSpeed = speed; // will be modified in interrupt
+	}
 	
+}
+
+// Echo back RXed character, confirm TX buffer is ready first
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void) {        // Vector 2 - RXIFG
+	while (!(IFG2 & UCA0TXIFG));             // USCI_A0 TX buffer ready?
+	char data = UCA0RXBUF;                  // TX -> RXed character
+	if (data != '\n'){
+		switch(data){
+				case 'l':
+					servoSetDuty(DUTY_LOWER_BOUND);
+					break;
+				case 'r':
+					servoSetDuty(DUTY_UPPER_BOUND);
+					break;
+				case 's':
+					servoSetDuty(DUTY_MID_BOUND);
+					break;
+				default:
+					speed = data;
+					break;
+			}
+	}
+	UCA0TXBUF = data;
 }
